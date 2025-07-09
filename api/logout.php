acf859c9436d348fe1c22315f0d3216e7deb5f67
0Lusket4s <162515779+0Lusket4s@users.noreply.github.com>
2025-07-09 10:54:46 -0300

    Update logout.php

--- api/logout.php
+++ api/logout.php
@@ -1,6 +1,7 @@
 <?php
-session_start();
-session_destroy();
+setcookie('usuario', '', time() - 3600, '/');
+setcookie('nivel', '', time() - 3600, '/');
 header('Location: login.php');
 exit();
+
 ?>
