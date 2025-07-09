54b7c04956c2b67fdfa5835272898939ee58b0c7
0Lusket4s <162515779+0Lusket4s@users.noreply.github.com>
2025-07-09 10:51:29 -0300

    Update dashboard.php

--- api/dashboard.php
+++ api/dashboard.php
@@ -1,12 +1,10 @@
 <?php
 
-session_start();
-var_dump($_SESSION);
-exit();
-if (!isset($_SESSION['usuario'])) {
+if (!isset($_COOKIE['usuario'])) {
     header('Location: login.php');
     exit();
 }
+
 
 ?>
 
