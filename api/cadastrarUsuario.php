942a48e7a5e49072350e76dfe6307137cd420d60
0Lusket4s <162515779+0Lusket4s@users.noreply.github.com>
2025-07-09 10:13:21 -0300

    Update cadastrarUsuario.php

--- api/cadastrarUsuario.php
+++ api/cadastrarUsuario.php
@@ -3,7 +3,7 @@ require_once 'conexao.php';
 
 $usuario = "lucas";
 $nome = "Administrador";
-$senha = "123";
+$senha = password_hash("123", PASSWORD_DEFAULT);
 $nivel = "Administrador";
 
 $stmt = $pdo->prepare("INSERT INTO usuarios (nome, usuario, senha, nivel_acesso) VALUES (:nome, :usuario, :senha, :nivel)");
